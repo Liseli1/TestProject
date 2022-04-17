542e338fa0b070900e3558b63213f21d7db8a58a
Liseli1 <993002404@qq.com>
2022-04-17 23:21:18 +0800

    test

--- src/TestJava.java
+++ src/TestJava.java
@@ -2,5 +2,10 @@ public class TestJava {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("ttttt");
+
+        System.out.println("ttttt");
+
+
+        System.out.println("ttttt");
     }
 }
